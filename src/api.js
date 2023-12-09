6b924c9a895468f0930197babedf0fae429cc6fe
Rakeshh <rakeshbhambhani7@gmail.com>
2023-12-09 23:24:04 +0530

    fixed-a-bug

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: process.env.REACT_APP_YT_API_KEY,
+    key: "AIzaSyD5tHH4XOWfcGc7TWaCDp4Ei1OfA5flDnQ",
   },
 });
 
